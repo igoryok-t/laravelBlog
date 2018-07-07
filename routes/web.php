4e2ce8fd1b886c700bf8a53cc60e0e75c00beab9
Ingvar <igoryok-t@yandex.ru>
2018-07-07 22:21:05 +0300

    Create admin dashboards

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+Route::group(['prefix'=>'admin', 'namespace' => 'Admin', 'middleware' => ['auth']],function (){
+    Route::get('/','DashboardController@dashboard')->name('admin.index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
